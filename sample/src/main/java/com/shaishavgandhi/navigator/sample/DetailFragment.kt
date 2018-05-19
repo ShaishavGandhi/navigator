352b8f0bda98a69882579668fb75835681d3c559
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-05-18 23:03:00 -0700

    Fix build
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailFragment.kt
+++ sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailFragment.kt
@@ -6,9 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.shaishavgandhi.navigato.sampler.R
 import com.shaishavgandhi.navigator.Extra
 
-import com.shaishavgandhi.navigator.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
