5f8476ed61ad0d2c4081bf84c2a24d54862f81b2
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-05-24 22:35:33 -0700

    Split out builders into own files
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailActivity.kt
+++ sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import android.support.annotation.Nullable
 import android.widget.TextView
 import android.widget.Toast
+import com.evernote.android.state.State
 import com.shaishavgandhi.navigato.sampler.R
 import com.shaishavgandhi.navigator.Extra
 import com.shaishavgandhi.navigator.Navigator
 
 class DetailActivity : AppCompatActivity() {
 
-    @Extra lateinit var userList: ArrayList<User>
-    @Extra @Nullable var points: Points? = null
+    @State @Extra lateinit var userList: ArrayList<User>
+    @State @Extra @Nullable var points: Points? = null
     @Extra var userId: Long? = null
     @Extra var source: String? = null
 
@@ -21,6 +22,7 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         Navigator.bind(this)
+
         Navigator.prepareDetailFragment(userList[0])
                 .bundle
         findViewById<TextView>(R.id.whatever).text = userList.first().name
