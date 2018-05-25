d657d7e4f721b9851f1309338767030a988cbdb6
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-05-24 22:37:18 -0700

    Remove state references
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailActivity.kt
+++ sample/src/main/java/com/shaishavgandhi/navigator/sample/DetailActivity.kt
@@ -5,15 +5,14 @@ import android.os.Bundle
 import android.support.annotation.Nullable
 import android.widget.TextView
 import android.widget.Toast
-import com.evernote.android.state.State
 import com.shaishavgandhi.navigato.sampler.R
 import com.shaishavgandhi.navigator.Extra
 import com.shaishavgandhi.navigator.Navigator
 
 class DetailActivity : AppCompatActivity() {
 
-    @State @Extra lateinit var userList: ArrayList<User>
-    @State @Extra @Nullable var points: Points? = null
+    @Extra lateinit var userList: ArrayList<User>
+    @Extra @Nullable var points: Points? = null
     @Extra var userId: Long? = null
     @Extra var source: String? = null
 
