9bc0b4ec83fd8554d9d03514880f75e36ff036c1
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-07-09 16:13:43 -0700

    Add docs
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- annotations/src/main/java/com/shaishavgandhi/navigator/Extra.java
+++ annotations/src/main/java/com/shaishavgandhi/navigator/Extra.java
@@ -6,8 +6,40 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
+/**
+ * Marks a field as an `extra`. Using this annotation will indicate
+ * to Navigator to generate `Builder` and `Binder` classes. `Builder`
+ * class will provide convenience methods to set the annotated field
+ * to the Bundle and the `Binder` does the binding of the Bundle data
+ * to the annotated fields.
+ *
+ * Fields annotated with {@link Extra} are non-null by default and will
+ * be added to the static factory method of the `Builder`. If the field
+ * is optional, then you should use the {@link android.support.Nullable}
+ * annotation (if you're using Java) or make the field nullable in Kotlin
+ * with the "?".
+ *
+ * Example:
+ * <pre>
+ *     public class MyActivity extends Activity {
+ *         {@literal @}Extra String message;
+ *         {@literal @}Nullable
+ *         {@literal @}Extra User user;
+ *     }
+ * </pre>
+ *
+ * @see Optional
+ */
 @Target(ElementType.FIELD)
 @Retention(SOURCE)
 public @interface Extra {
+    /**
+     * The custom key for the annotated field. This helps
+     * with backward compatibility, where you can incrementally
+     * move to Navigator. If no key is specified, the field
+     * name is taken as the key.
+     *
+     * @return The key of the field.
+     */
     String key() default "";
 }
--- annotations/src/main/java/com/shaishavgandhi/navigator/Optional.java
+++ annotations/src/main/java/com/shaishavgandhi/navigator/Optional.java
@@ -6,6 +6,17 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
+/**
+ * Marks annotated field as "Optional" to Navigator. This implies
+ * that the generated `Builder` class will not add it to the
+ * static factory method, but will create a builder method to set
+ * the field. This is especially useful when dealing with primitive
+ * types. It doesn't make sense to mark primitives as Nullable.
+ * If you want your primitive field to be Nullable, annotate with
+ * {@link Optional}
+ *
+ * @see Extra
+ */
 @Target(ElementType.FIELD)
 @Retention(SOURCE)
 public @interface Optional {
