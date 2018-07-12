6306758cb36b2561e9c25da97bf5909576f92c7d
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-07-11 22:52:48 -0700

    Add object type mapping + add test case
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- sample/src/main/java/com/shaishavgandhi/navigator/sample/SampleActivity.java
+++ sample/src/main/java/com/shaishavgandhi/navigator/sample/SampleActivity.java
@@ -11,6 +11,8 @@ import com.shaishavgandhi.navigator.Extra;
 public class SampleActivity extends AppCompatActivity {
 
     @Extra String message;
+    @Extra Short javaShort;
+    Long points = -1L;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
